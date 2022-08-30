2d14a290e14a3bb445c37084c895536143b3fc97
Aditya <aditya.gupta@thinkitive.com>
2022-08-30 12:24:13 +0530

    Connect with database

--- pages/blog.js
+++ pages/blog.js
@@ -1,4 +1,25 @@
-function Blog() {
-  return <h1>Blog page</h1>;
+import { getSession, useSession } from "next-auth/react";
+function Blog({ data }) {
+  const { status } = useSession();
+  console.log(status);
+  return <h1>Blog page - {data}</h1>;
 }
 export default Blog;
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+  console.log(session);
+  if (!session)
+    return {
+      redirect: {
+        destination: `/api/auth/signin?callbackUrl=http://localhost:3000/blog`,
+        permanent: false,
+      },
+    };
+  return {
+    props: {
+      session,
+      data: session ? "List of 100 perzonalized blogs" : "List of free blogs",
+    },
+  };
+}
